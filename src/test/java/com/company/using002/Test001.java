0c03054d33f53fe638b6e2b663afddac76b96d41
Jins Lee <seung6570@gmail.com>
2024-02-13 16:34:54 +0900

    ##0213 test

--- src/test/java/com/company/using002/Test001.java
+++ src/test/java/com/company/using002/Test001.java
@@ -6,6 +6,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -19,6 +23,10 @@ import com.company.api.Random;
 import com.company.dto.UserDto;
 import com.company.dto.UserVoDto;
 import com.company.service.UserService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -74,7 +82,7 @@ public class Test001 {
 		urlBuilder.append("/" +  URLEncoder.encode("json","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
 		urlBuilder.append("/" + URLEncoder.encode("tbHanyangPoi","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
 		urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
-		urlBuilder.append("/" + URLEncoder.encode("65","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+		urlBuilder.append("/" + URLEncoder.encode("3","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
 		// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
 		
 		// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
@@ -100,7 +108,27 @@ public class Test001 {
 		}
 		rd.close();
 		conn.disconnect();
-		System.out.println(sb.toString());
+		String infoResult=sb.toString();
+		System.out.println("@@@@@@@@"+ infoResult);
+		
+		JsonParser parser=new JsonParser();
+		JsonObject job=(JsonObject) parser.parse(infoResult);
+		JsonArray read=(JsonArray) job.getAsJsonObject("tbHanyangPoi").getAsJsonArray("row");
+		System.out.println(read);
+		List<Map<String, Object>> result=new ArrayList<>();
+			for(JsonElement one : read) {
+			JsonObject read_one=one.getAsJsonObject();
+			String ITRST_BRNCH_NO=read_one.get("ITRST_BRNCH_NO").getAsString();
+			String KORN_TTL=read_one.get("KORN_TTL").getAsString();
+			String KORN_CONTS=read_one.get("KORN_CONTS").getAsString();
+			Map<String, Object> map=new HashMap<>();
+						
+			map.put("ITRST_BRNCH_NO", ITRST_BRNCH_NO);
+			map.put("KORN_TTL", KORN_TTL);
+			map.put("KORN_CONTS", KORN_CONTS);
+			result.add(map);
+			}
+		System.out.println("last@@@@ : "+result.get(0).get("ITRST_BRNCH_NO"));
 	}
 		
 	
